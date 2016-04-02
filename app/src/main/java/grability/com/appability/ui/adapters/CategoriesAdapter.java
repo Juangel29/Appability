75864f11d51c4b88aab7dd2cd354be35fdb68656
Juan Angel <juan.ardila@globant.com>
2016-04-02 14:15:02 -0500

    Add onItemTouch event to categories adapter

--- app/src/main/java/grability/com/appability/ui/adapters/CategoriesAdapter.java
+++ app/src/main/java/grability/com/appability/ui/adapters/CategoriesAdapter.java
@@ -1,7 +1,10 @@
 package grability.com.appability.ui.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,12 +15,22 @@ import io.realm.RealmResults;
  *
  * Created by juanangelardila on 4/1/16.
  */
-public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesViewHolder> {
+public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesViewHolder> implements RecyclerView.OnItemTouchListener{
+
+    private GestureDetector gestureDetector;
+    private OnItemClickListener onItemClickListener;
 
     private RealmResults<Category> categories;
 
-    public CategoriesAdapter(RealmResults<Category> categories) {
+    public CategoriesAdapter(Context context, RealmResults<Category> categories) {
         this.categories = categories;
+
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
     }
 
     @Override
@@ -36,5 +49,30 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesViewHolder
     @Override
     public int getItemCount() {
         return categories.size();
+    }
+
+
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+        View childView = rv.findChildViewUnder(e.getX(), e.getY());
+        if (childView != null && onItemClickListener != null && gestureDetector.onTouchEvent(e)) {
+            onItemClickListener.onItemClick(childView, rv.getChildAdapterPosition(childView));
+        }
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) { }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) { }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 }
--- app/src/main/java/grability/com/appability/ui/fragments/CategoriesFragment.java
+++ app/src/main/java/grability/com/appability/ui/fragments/CategoriesFragment.java
@@ -4,50 +4,36 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import grability.com.appability.R;
 
 
 public class CategoriesFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    @Bind(R.id.rvCategories) RecyclerView rvCategories;
 
     private OnFragmentInteractionListener mListener;
 
     public CategoriesFragment() {
-        // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
@@ -56,8 +42,7 @@ public class CategoriesFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -67,16 +52,6 @@ public class CategoriesFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
